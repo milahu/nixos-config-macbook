033090044e923d3e49ed340fcab2fa3fdc37693b
user <>
2023-05-14 20:04:04 +0200

    add substituter nix-community.cachix.org

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,14 @@ in
   nix.settings.auto-optimise-store = true;
   # TODO automate garbage collect
 
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+    "https://cache.nixos.org/"
+  ];
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
   nixpkgs.overlays = [
     inputs.nur.overlay
   ];
