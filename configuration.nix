da6b45f24f68884103827e81b7cbe49387645152
user <>
2023-05-15 15:33:36 +0200

    fix steam

--- configuration.nix
+++ configuration.nix
@@ -278,6 +278,14 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # steam
+  # fix: glXChooseVisual failed
+  hardware = {
+    opengl.driSupport32Bit = true;
+    # FIXME error: attribute 'pulseaudio' already defined
+    #pulseaudio.support32Bit = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
