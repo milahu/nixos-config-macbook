2f63f3e7a67d144c218a5623bdb18f90078d17f3
user <>
2023-05-15 11:16:05 +0200

    wine

--- packages.nix
+++ packages.nix
@@ -64,4 +64,20 @@ with pkgs; [
   libjpeg # jpegtran
   screen
   yt-dlp # youtube downloader
+
+  # wine: run windows programs on linux
+  # support both 32- and 64-bit applications
+  #wineWowPackages.stable
+
+  # support 32-bit only
+  #wine
+
+  # support 64-bit only
+  #(wine.override { wineBuild = "wine64"; })
+
+  # wine-staging (version with experimental features)
+  wineWowPackages.staging
+
+  # winetricks (all versions)
+  winetricks
 ]
