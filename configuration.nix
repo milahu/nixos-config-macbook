5be838866372d10d15e6a458a6b042909192d41c
user <>
2023-05-15 15:33:19 +0200

    sshd

--- configuration.nix
+++ configuration.nix
@@ -255,7 +255,8 @@
 
   # Enable the OpenSSH daemon.
   services.openssh = {
-    #enable = true;
+    enable = true;
+    openFirewall = true;
     # Forbid root login through SSH.
     settings.PermitRootLogin = "no";
     # Use keys only. Remove if you want to SSH using password (not recommended)
